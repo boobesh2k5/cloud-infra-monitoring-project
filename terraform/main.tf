2ce9315cd2f573897fe7a699065144f95396dfeb
root <root@ip-172-31-22-237.eu-north-1.compute.internal>
2026-09-09 16:59:06 +0000

    Add frontend build files and update infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -478,7 +478,7 @@ resource "aws_eks_node_group" "main" {
   capacity_type = "ON_DEMAND"
 
   scaling_config {
-    desired_size = 2
+    desired_size = 3
     min_size     = 2
     max_size     = 3
   }
